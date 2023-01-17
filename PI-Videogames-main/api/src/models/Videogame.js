e738d1b70ad300e4f9e338a8947f526528419c76
Edwards0307 <edawrds.ardila03@gnail.com>
2023-01-16 19:37:17 -0500

    Creacion de Modelos

--- PI-Videogames-main/api/src/models/Videogame.js
+++ PI-Videogames-main/api/src/models/Videogame.js
@@ -1,12 +1,44 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('videogame', {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
+  sequelize.define(
+    "videogame",
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+        allowNull: false,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      released: {
+        type: DataTypes.STRING,
+      },
+      rating: {
+        type: DataTypes.FLOAT,
+      },
+      platforms: {
+        type: DataTypes.ARRAY(DataTypes.STRING),
+        allowNull: false,
+      },
+      img: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      created: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: true,
+      },
     },
-  });
+    { timestamps: false }
+  );
 };
