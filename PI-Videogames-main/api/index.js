3d068314024f9e71d3aa088138fb5c0c09058d20
Edwards Ardila <edwards.ardila03@gmail.com>
2023-03-20 16:33:36 -0500

    Actualizacion deploy

--- PI-Videogames-main/api/index.js
+++ PI-Videogames-main/api/index.js
@@ -20,11 +20,13 @@
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 const { findGenresBdd } = require("./src/helpers/getGenres");
+require("dotenv").config();
+const { PORT } = process.env;
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
-  server.listen(3001, () => {
-    console.log("%s listening at 3001"); // eslint-disable-line no-console
+  server.listen(PORT, () => {
+    console.log(` listening at ${PORT}`); // eslint-disable-line no-console
     findGenresBdd();
   });
 });
